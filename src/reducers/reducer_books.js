26b296777feea0834f0a7d13df0460659efc6bd1
ctur <cturesoy@gmail.com>
2018-01-23 13:00:51 +0300

    Starting book store app.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The Good Parts' },
+        { title: 'HP'},
+        { title: 'TDT'},
+        { title: 'Eloquent ruby'},
+    ]
+}

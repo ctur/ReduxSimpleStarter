c089fdb5b96b2cf3f9243ea52164ccb4a58a7225
ctur <cturesoy@gmail.com>
2018-01-23 17:46:37 +0300

    Actions and reducers added.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: 'HP'},
-        { title: 'TDT'},
-        { title: 'Eloquent ruby'},
+        { title: 'Javascript: The Good Parts', pages: 1010 },
+        { title: 'HP', pages: 400},
+        { title: 'TDT', pages: 250},
+        { title: 'Eloquent ruby', pages:10},
     ]
 }
